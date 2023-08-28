a32d2366609c388eeaf401651844e29af4e3a717
sumanhong <suman0204@naver.com>
2023-08-28 09:11:02 +0900

    Initial Commit

---
+++ SeSACPhotoGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACPhotoGram
+//
+//  Created by 홍수만 on 2023/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
